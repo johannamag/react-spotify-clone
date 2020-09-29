a0263ef43f2b2f8a2539aa0d2048f8c43f410c95
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-29 09:00:38 +0200

    Added Datalayer

---
+++ src/reducer.js
@@ -0,0 +1,27 @@
+export const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+  token:
+    "BQBn3BnBTB7ycWm9jd3jJtIiVXKrb1NOH7FB20fuD_a6hwyq5bsG3YRt03kkwrTK2nqprj_xSgYwNrNscQT4rYi95h8g-cVfQVphJ9K5QrzpUIeO02alOpUoCeu2ngu6gkwa10CVfn0u4zGmp5L9SlQ_1vJFEM5Kww",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
